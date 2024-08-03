45774a82253d0b3438915cb30f32e4b919aa9441
martinromario55 <martinromario55@gmail.com>
2024-08-03 17:03:42 +0300

    Securing Screens according to User Auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Link } from 'expo-router'
+import { Link, Redirect } from 'expo-router'
 import Button from '@/components/Button'
+import { useAuth } from '@/providers/AuthProvider'
+import { supabase } from '@/lib/supabase'
 
 const index = () => {
+  const { session, loading, isAdmin } = useAuth()
+
+  if (loading) {
+    return <ActivityIndicator />
+  }
+
+  // console.log(session)
+  if (!session) {
+    return <Redirect href={'/sign-in'} />
+  }
+
+  if (!isAdmin) {
+    return <Redirect href={'/(user)'} />
+  }
+
   return (
     <View style={styles.container}>
       <Link href={'/(user)'} asChild>
@@ -17,6 +34,8 @@ const index = () => {
       <Link href={'/sign-in'} asChild>
         <Button text="Sign In" />
       </Link>
+
+      <Button text="Sign out" onPress={() => supabase.auth.signOut()} />
     </View>
   )
 }
